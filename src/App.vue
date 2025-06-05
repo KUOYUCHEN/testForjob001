4e5929d250cf028666b02460c9073eae530c8488
ACE Kylo <kylo_chen@acebiotek.com>
2025-06-05 15:38:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<!-- @format -->
+
+<script setup lang="ts">
+import test from "./components/test.vue";
+</script>
+
+<template>
+    <test class="wrap" />
+</template>
+
+<style scoped></style>
---
+++ src/components/test.vue
@@ -0,0 +1,32 @@
+<!-- @format -->
+
+<script setup lang="ts"></script>
+
+<template>
+    <div class="wrap">
+        <div class="sidebar">
+            <h3>白頭翁不吃小米</h3>
+        </div>
+        <div class="content">
+            <div class="picture"></div>
+            <div class="description"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.wrap {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+}
+.sidebar {
+    width: 20%;
+    background-color: #f0f0f0;
+}
+.content {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+}
+</style>
